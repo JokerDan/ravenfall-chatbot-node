3506617df8bd56f37cf92ca08e85a8dddf2ccf8c
JokerDan <danj.parkes@gmail.com>
2019-10-03 21:22:09 +0100

    Fix an issue with getting broadcaster
    We can't get/check the broadcaster badges if there are none returned to
    check for.

--- src/util/user.js
+++ src/util/user.js
@@ -1,10 +1,13 @@
 module.exports = {
   isBroadcaster: function(ctx) {
+    if (!ctx.badges) {
+      return false;
+    }
     return Boolean(ctx.badges.broadcaster);
   },
 
   isMod: function(ctx) {
-    return (Boolean(ctx.mod) || isBroadcaster(ctx));
+    return (Boolean(ctx.mod) || this.isBroadcaster(ctx));
   },
 
   isSub: function(ctx) {
