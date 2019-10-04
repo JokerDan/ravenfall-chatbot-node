b7eea0921a12fc0a05a3921a8d5af309421970b8
JokerDan <danj.parkes@gmail.com>
2019-10-04 22:18:47 +0100

    Remove our output for startRaid

--- src/commands/startRaid.js
+++ src/commands/startRaid.js
@@ -1,4 +1,4 @@
 export default function(ctx) {
     ctx.dispatchEvent('raid_force', JSON.stringify(ctx.player));
-    ctx.twitchClient.say(ctx.channel, `${ctx.userstate['display-name']} has STARTED a RAID!`);
+    // ctx.twitchClient.say(ctx.channel, `${ctx.userstate['display-name']} has STARTED a RAID!`);
 }
