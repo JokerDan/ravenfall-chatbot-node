2d9b7d88cabb355c02d6df8c0d6950a30efd0ddc
JokerDan <danj.parkes@gmail.com>
2019-10-04 21:06:02 +0100

    Player stats supports single skill query

--- src/commands/playerStats.js
+++ src/commands/playerStats.js
@@ -1,8 +1,9 @@
 export default function(ctx) {
 
-    let statRequest;
-
-    statRequest = new StatsRequest(ctx.player, '');
+    let statRequest = new StatsRequest(ctx.player, '');
+    if (ctx.args[0] != undefined) {
+        statRequest.Skill = String(ctx.args[0]);
+    }
     ctx.dispatchEvent('player_stats', JSON.stringify(statRequest));
 }
 
